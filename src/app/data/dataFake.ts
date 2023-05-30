90d42ae7a8f74d7ed05eb5471cb9c6d0944cf3bd
leo <tlsleonardosilva@gmail.com>
2023-05-30 15:01:09 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"Marvel Studios",
+        "description":"ola",
+        "photoCover":"https://www.komar.de/media/cms/fileadmin/user_upload/Category/Fototapeten/Marvel/komar-fototapeten-marvel.jpg"
+    },
+    {
+        "id":"2",
+        "title":"Marvel Avengers",
+        "description":"ola",
+        "photoCover":"https://tm.ibxk.com.br/2022/05/04/04181551056727.jpg?ims=1200x675"
+    },
+    {
+        "id":"3",
+        "title":"Guardians Galaxy",
+        "description":"ola",
+        "photoCover":"https://media.seudinheiro.com/cdn-cgi/image/fit=contain,width=715&,format=auto/uploads/2023/02/Guardians-of-the-Galaxy-Vol.-3-715x402.jpg"
+    },
+    {
+        "id":"4",
+        "title":"Doutor Estranho",
+        "description":"ola",
+        "photoCover":"https://correio-cdn2.cworks.cloud/fileadmin/_processed_/f/5/csm_doutor_estranho_2_9db0e64d73.jpg"
+    }
+]
